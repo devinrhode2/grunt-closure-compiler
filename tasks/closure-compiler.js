78260b07ed8a0eaec69e83fce556fa160a7d0ada
Devin Rhode <devinrhode2@gmail.com>
2012-11-21 15:44:02 -0800

    mid-way on feature to simple compile select files + concatenate

--- tasks/closure-compiler.js
+++ tasks/closure-compiler.js
@@ -44,8 +44,6 @@ module.exports = function(grunt) {
       return false;
     }
 
-    var command = 'java -jar ' + closurePath + '/build/compiler.jar';
-
     data.js = grunt.file.expandFiles(data.js);
 
     // Sanitize options passed.
@@ -56,42 +54,59 @@ module.exports = function(grunt) {
     }
 
     // Build command line.
-    command += ' --js ' + data.js.join(' --js ');
+    var buildCommand = function buildCommand(js, output) {
+      var command = 'java -jar ' + closurePath + '/build/compiler.jar';
 
-    if (data.jsOutputFile) {
-      command += ' --js_output_file ' + data.jsOutputFile;
-      reportFile = data.jsOutputFile + '.report.txt';
-    }
+      // Indentation will happen in another commit
+      if (data.simple || data.advanced) {
+        //remove each simple script from main compilation
+        if (data.advanced) throw new Error('the advanced option is not yet fully supported, please fork and implement it!');
 
-    if (data.externs) {
-      data.externs = grunt.file.expandFiles(data.externs);
-      command += ' --externs ' + data.externs.join(' --externs ');
-
-      if (!data.externs.length) {
-        delete data.externs;
+        //forEach data.simple or data.advanced script...
+        data[(data.simple ? 'simple' : 'advanced')].forEach(function(script){
+          data.js.remove(script);
+        });
       }
-    }
+      command += ' --js ' + js.join(' --js ');
 
-    if (data.options.externs) {
-      data.options.externs = grunt.file.expandFiles(data.options.externs);
-
-      if (!data.options.externs.length) {
-        delete data.options.externs;
+      if (data.jsOutputFile) {
+        command += ' --js_output_file ' + data.jsOutputFile;
+        reportFile = data.jsOutputFile + '.report.txt';
       }
-    }
 
-    for (var directive in data.options) {
-      if (Array.isArray(data.options[directive])) {
-        command += ' --' + directive + ' ' + data.options[directive].join(' --' + directive + ' ');
-      } else if (data.options[directive] === undefined || data.options[directive] === null) {
-        command += ' --' + directive;
-      } else {
-        command += ' --' + directive + ' ' + String(data.options[directive]);
+      if (data.externs) {
+        data.externs = grunt.file.expandFiles(data.externs);
+        command += ' --externs ' + data.externs.join(' --externs ');
+
+        if (!data.externs.length) {
+          delete data.externs;
+        }
       }
-    }
+
+      if (data.options.externs) {
+        data.options.externs = grunt.file.expandFiles(data.options.externs);
+
+        if (!data.options.externs.length) {
+          delete data.options.externs;
+        }
+      }
+
+      for (var directive in data.options) {
+        if (Array.isArray(data.options[directive])) {
+          command += ' --' + directive + ' ' + data.options[directive].join(' --' + directive + ' ');
+        } else if (data.options[directive] === undefined || data.options[directive] === null) {
+          command += ' --' + directive;
+        } else {
+          command += ' --' + directive + ' ' + String(data.options[directive]);
+        }
+      }
+      
+      return command;
+    };
 
     // Minify WebGraph class.
-    exec(command, { maxBuffer: data.maxBuffer*1024 }, function(err, stdout, stderr) {
+    options = { maxBuffer: data.maxBuffer*1024 };
+    exec(buildCommand(data.js, data.jsOutputFile), options, function(err, stdout, stderr) {
       if (err) {
         grunt.warn(err);
         done(false);
@@ -101,29 +116,41 @@ module.exports = function(grunt) {
         grunt.log.writeln(stdout);
       }
 
-      // If OK, calculate gzipped file size.
-      if (reportFile.length) {
-        var min = fs.readFileSync(data.jsOutputFile, 'utf8');
-        min_info(min, function(err) {
-          if (err) {
-            grunt.warn(err);
-            done(false);
-          }
+      exec(buildCommand(data.simple, data.), options, function(err, stdout, stderr) {
+        if (err) {
+          grunt.warn(err);
+          done(false);
+        }
 
-          // Write compile report to a file.
-          fs.writeFile(reportFile, stderr, function(err) {
+        if (stdout) {
+          grunt.log.writeln(stdout);
+        }
+
+        // If OK, calculate gzipped file size.
+        if (reportFile.length) {
+          var min = fs.readFileSync(data.jsOutputFile, 'utf8');
+          min_info(min, function(err) {
             if (err) {
               grunt.warn(err);
               done(false);
             }
 
-            grunt.log.writeln('A report is saved in ' + reportFile + '.');
-            done();
+            // Write compile report to a file.
+            console.log('write file path:', reportFile);
+
+            fs.writeFile(reportFile, stderr, function(err) {
+              if (err) {
+                grunt.warn(err);
+                done(false);
+              }
+
+              grunt.log.writeln('A report is saved in ' + reportFile + '.');
+              done();
+            });
+
           });
-
-        });
-      }
-
+        }
+      });
     });
 
   });

df9c85b124e0dbc13b0c0abd9d08cccdc3d3eb7e
Devin Rhode <devinrhode2@gmail.com>
2012-11-20 22:01:31 -0800

    added array.prototype.remove

--- tasks/closure-compiler.js
+++ tasks/closure-compiler.js
@@ -6,6 +6,18 @@ module.exports = function(grunt) {
       fs = require('fs'),
       gzip = require('zlib').gzip;
 
+  // From http://stackoverflow.com/a/3955096
+  Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+      what = a[--L];
+      while ((ax = this.indexOf(what)) !== -1) {
+        this.splice(ax, 1);
+      }
+    }
+    return this;
+  };
+
   // ==========================================================================
   // TASKS
   // ==========================================================================
